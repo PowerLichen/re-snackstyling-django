7a1581dff347b0347d2901e83fbdcf596b7beb9f
PowerLichen <ms.choe015@gmail.com>
2023-04-27 23:33:27 +0900

    Add clothing tag update view code

--- api/clothing/serializers.py
+++ api/clothing/serializers.py
@@ -15,26 +15,52 @@ class ClothingSerializer(serializers.ModelSerializer):
         model = Clothing
         fields = ["id", "user", "image", "created"]
 
+
 class ClothingCreateSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    
+
     class Meta:
         model = Clothing
         fields = ["id", "user", "image"]
-        
+
+
 class ClothingRetrieveSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
     tags = serializers.StringRelatedField(many=True)
-    
+
     class Meta:
         model = Clothing
         fields = ["id", "user", "image", "category", "tags", "created"]
-        
+
     def get_category(self, obj):
         return obj.tags.filter(type="CTG").values_list("name", flat=True)
-    
+
+
 class ClothingUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clothing
         fields = ["image", "tags"]
-    
+
+
+class TagListSerializer(serializers.ListSerializer):
+    def update(self, instance, validated_data):
+        data_mapping = {item["id"]: item for item in validated_data}
+
+        result = list()
+        for one in instance:
+            obj = self.child.update(one, data_mapping[one.id])
+            result.append(obj)
+
+        return result
+
+    def get_id_set(self):
+        result = {item["id"] for item in self.validated_data}
+        return result
+
+
+class ClothingTagUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clothing
+        fields = ["id", "tags"]
+        extra_kwargs = {'id': {'read_only': False}}
+        list_serializer_class = TagListSerializer
--- api/clothing/views.py
+++ api/clothing/views.py
@@ -5,19 +5,22 @@ Clothing views.py
 @author 최민수
 @create 2023-04-17
 """
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.clothing.serializers import ClothingSerializer
 from api.clothing.serializers import ClothingCreateSerializer
 from api.clothing.serializers import ClothingRetrieveSerializer
 from api.clothing.serializers import ClothingUpdateSerializer
+from api.clothing.serializers import ClothingTagUpdateSerializer
 from model.clothing.models import Clothing
 
 
 class ClothingViewSet(ModelViewSet):
     queryset = Clothing.active_objects.all()
     serializer_class = ClothingSerializer
-    
+
     def get_serializer_class(self):
         if self.action == "create":
             return ClothingCreateSerializer
@@ -27,9 +30,24 @@ class ClothingViewSet(ModelViewSet):
             return ClothingRetrieveSerializer
         if self.action in ["update", "partial_update"]:
             return ClothingUpdateSerializer
-        
+        if self.action == "multiple_tag_update":
+            return ClothingTagUpdateSerializer
+
         return super().get_serializer_class()
-    
+
     def perform_destroy(self, instance):
         instance.is_active = False
         instance.save()
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @action(detail=False, methods=["patch"], url_path="tag")
+    def multiple_tag_update(self, request, *args, **kwargs):
+        target_pk_list = [item["id"] for item in request.data]
+        instance = self.get_queryset().filter(id__in=target_pk_list)
+        serializer = self.get_serializer(
+            instance, data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
